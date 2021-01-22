1f58f506b0c6aa55ed736512f15834b8cb70bb0a
zongronghuang <zongrong.h886@gmail.com>
2021-01-22 17:54:28 +0800

    Split components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  background-color: #ededed;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center; 
+`
+
+const WeatherCard = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: 0 1px 3px 0 #999999;
+  background-color: #f9f9f9;
+  box-sizing: border-box;
+  padding: 30px 15px;
+`
+
+const Location = styled.div`
+  font-size: 28px;
+  color: #212121;
+  margin-bottom: 20px;
+`
+
+const Description = styled.div`
+font-size: 16px;
+  color: #828282;
+  margin-bottom: 30px;
+`
+
+const CurrentWeather = styled.div`
+display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`
+
+const Temperature = styled.div`
+color: #757575;
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`
+const Celsius = styled.div`
+font-weight: normal;
+  font-size: 42px;
+`
+
+const AirFlow = styled.div`
+display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  margin-bottom: 20px;
+`
+
+const Rain = styled.div`
+display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+`
+
+const Refresh = styled.div`
+ position: absolute;
+  right: 15px;
+  bottom: 15px;
+  font-size: 12px;
+  display: inline-flex;
+  align-items: flex-end;
+  color: #828282;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <WeatherCard>
+        <Location>台北市</Location>
+        <Description>多雲時晴</Description>
+        <CurrentWeather>
+          <Temperature>
+            23 <Celsius>°C</Celsius>
+          </Temperature>
+        </CurrentWeather>
+        <AirFlow> 23 m/h </AirFlow>
+        <Rain> 48% </Rain>
+        <Refresh> 最後觀測時間：上午 12:03 </Refresh>
+      </WeatherCard>
+    </Container>
   );
 }
 
